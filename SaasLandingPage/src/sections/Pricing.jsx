00d4ca54c4f3662696a6dc6507a46a3c0af220ea
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-10-13 23:44:41 +0530

    Pricing update

--- SaasLandingPage/src/sections/Pricing.jsx
+++ SaasLandingPage/src/sections/Pricing.jsx
@@ -1,23 +1,38 @@
 import clsx from "clsx";
 import React, { useState } from "react";
 import { Element } from "react-scroll";
+import { plans } from "../constants/store";
 
 function Pricing() {
 	const [monthly, setMonthly] = useState(false);
 	return (
 		<section>
 			<Element name="pricing">
-				<div className="container">
+				<div className="container ">
 					<div
 						className="max-w-960 max-xl:max-w-4xl border-2 pricing-head_before relative mx-auto 
                     border-l border-r max-lg:border-none border-s2 bg-s1/50 pb-40 max-md:pb-32 pt-28 max-md:pt-16 z-3"
 					>
 						<h3
 							className="h3 mx-auto max-lg:h4 max-md:h5 capitalize text-center text-p4 
-                        mb-14 max-:mb-11 max-w-lg max-sm:max-w-sm"
+                        mb-14 max-:mb-11 max-w-lg max-sm:max-w-sm z-10"
 						>
 							flexible pricing for teams of all sizes
 						</h3>
+						<div className=" pricing-bg ">
+							<img
+								src="images/bg-outlines.svg"
+								width={960}
+								height={380}
+								alt="outline"
+								className=" relative -z-50"
+							/>
+							<img
+								src="images/bg-outlines-fill.png"
+								className=" opacity-5 absolute inset-0 mix-blend-soft-light"
+								alt=""
+							/>
+						</div>
 						<div
 							className=" relative z-4 mx-auto flex border-[2px] border-s3/25 rounded-3xl
                          bg-s1/50 p-2  backdrop-blur-[6px] w-[375px] max-md:w-[310px]"
@@ -40,15 +55,40 @@ function Pricing() {
 									!monthly && "translate-x-full"
 								)}
 							></div>
-							<div className="pricing-bg">
-								<img
-									src="images/bg-outlines.svg"
-									width={960}
-									height={380}
-									alt="outline"
-								/>
-							</div>
 						</div>
+					</div>
+					{/* Pricing section */}
+					<div
+						className=" scroll-hide relative z-2 -mt-12 flex items-start 
+						max-xl:gap-5 max-xl:overflow-auto max-xl:pt-6 justify-center"
+					>
+						{plans.map((plan, index) => (
+							<div
+								key={plan.id}
+								className=" pricing-plan_first pricing-plan_last pricing-plan_odd pricing-plan_even
+								 relative border-2 p-7 max-xl:min-w-80 max-lg:rounded-3xl xl:w-[calc(33.33%+2px)]"
+							>
+								{index === 1 && (
+									<div className=" g4 absolute h-330 left-0 right-0 top-0 z-1 rounded-t-3xl"></div>
+								)}
+								<div
+									className={clsx(
+										"absolute left-0 right-0 z-2  flex items-center justify-center",
+										index == 1 ? " -top-6" : " -top-6 xl:-top-11"
+									)}
+								>
+									<img
+										src={plan.logo}
+										alt={plan.title}
+										className={clsx(
+											"object-contain drop-shadow-2xl",
+											index == 1 ? "size-[120px]" : "size-[88px]"
+										)}
+									/>
+								</div>
+								<div className={clsx()}></div>
+							</div>
+						))}
 					</div>
 				</div>
 			</Element>
